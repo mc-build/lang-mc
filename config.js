96cdfe2a394bd4ea53fbd103ee25837969360626
IanSSenne <IanSSenne@gmail.com>
2020-09-07 05:40:25 -0700

    initial

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    dev: true,
+    header: "#built using mc-build (https://github.com/IanSSenne/mcbuild)",
+    internalScoreboard: "LANG_MC_INTERNAL",
+    rootNamespace: null
+};
