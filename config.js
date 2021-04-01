ff9800517bc22691a4ac13fb0e44ceedc95b1072
Samuel Bruin <67058024+Ancientkingg@users.noreply.github.com>
2021-04-01 08:54:53 +0200

    April Fools!

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     dev: true,
     header: "#built using mc-build (https://github.com/mc-build/mc-build)",
     internalScoreboard: "LANG_MC_INTERNAL",
-    rootNamespace: null
+    rootNamespace: null,
+    fools: true
 };
