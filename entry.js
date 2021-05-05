7d5e85d2845311d4144297a732656e0102027e2f
IanSSenne <48780301+IanSSenne@users.noreply.github.com>
2021-05-05 10:46:29 -0700

    Update entry.js

--- entry.js
+++ entry.js
@@ -779,7 +779,7 @@ consumer.Generic = list({
           `scoreboard players set #until_${_id} ${CONFIG.internalScoreboard} 0`
         );
         untilFunc.addCommand(`execute ${cond} run scoreboard players set #until_${_id} ${CONFIG.internalScoreboard} 1`);
-        untilFunc.addCommand(`execute if score #until_${_id} ${CONFIG.internalScoreboard} matches 1 run function ${call.getReference()}`);
+        untilFunc.addCommand(`execute if score #until_${_id} ${CONFIG.internalScoreboard} matches 1 run function ${call}`);
         untilFunc.addCommand(
           `execute if score #until_${_id} ${CONFIG.internalScoreboard} matches 0 run schedule function $block ${time}`
         );
