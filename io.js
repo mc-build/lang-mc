ddc858a2b5888ffb879e053c55ad7d045df0cf58
SnaveSutit <snavesutit@gmail.com>
2023-01-30 12:26:05 -0500

    Code formatting

--- io.js
+++ io.js
@@ -1,189 +1,175 @@
-const path = require("path");
+const path = require('path')
 
-const CompilerError = require("!errors/CompilerError");
-const CONFIG = require("!config/mc");
-const File = require("!io/File");
-const { evaluateCodeWithEnv } = require("./code-runner");
-const crypto = require("crypto");
+const CompilerError = require('!errors/CompilerError')
+const CONFIG = require('!config/mc')
+const File = require('!io/File')
+const { evaluateCodeWithEnv } = require('./code-runner')
+const crypto = require('crypto')
 
-let env;
-let _fakefs = new Set();
+let env
+let _fakefs = new Set()
 
 function evaluate(line) {
-  if (line.indexOf("<%") > -1 && line.indexOf("%>")) {
-    const template = line
-      .replace(/\${/g, '${"${"}')
-      .replace(/\\/g, "\\\\")
-      .replace(/<%/g, "${")
-      .replace(/%>/g, "}")
-      .replace(/\`/g, "\\`");
-    try {
-      return evaluateCodeWithEnv("return `" + template + "`", env);
-    } catch (e) {
-      console.log(e);
-      throw new CompilerError(`invalid template literal '${template}'`);
-    }
-  }
-  return line;
+	if (line.indexOf('<%') > -1 && line.indexOf('%>')) {
+		const template = line
+			.replace(/\${/g, '${"${"}')
+			.replace(/\\/g, '\\\\')
+			.replace(/<%/g, '${')
+			.replace(/%>/g, '}')
+			.replace(/\`/g, '\\`')
+		try {
+			return evaluateCodeWithEnv('return `' + template + '`', env)
+		} catch (e) {
+			console.log(e)
+			throw new CompilerError(`invalid template literal '${template}'`)
+		}
+	}
+	return line
 }
 
 class MultiFile {
-  constructor(file) {
-    this.segments = {};
-  }
-  set(id, func) {
-    this.segments[id] = this.segments[id] || [];
-    this.segments[id].push(func);
-  }
-  values() {
-    return Object.values(this.segments).flat();
-  }
-  valuesFor(key) {
-    return Object.values(this.segments[key] || {}).flat(Infinity);
-  }
-  reset(file) {
-    delete this.segments[file];
-  }
+	constructor(file) {
+		this.segments = {}
+	}
+	set(id, func) {
+		this.segments[id] = this.segments[id] || []
+		this.segments[id].push(func)
+	}
+	values() {
+		return Object.values(this.segments).flat()
+	}
+	valuesFor(key) {
+		return Object.values(this.segments[key] || {}).flat(Infinity)
+	}
+	reset(file) {
+		delete this.segments[file]
+	}
 }
 
 class MultiFileTag {
-  constructor(path) {
-    this.segments = {};
-    this.file = new File();
-    this.file.setPath(path);
-    this.current = false;
-  }
-  set(id, func) {
-    if (!this.current) {
-      this.file.confirm();
-      this.current = true;
-    }
-    this.segments[id] = this.segments[id] || [];
-    this.segments[id].push(func);
-    this.file.setContents(
-      JSON.stringify({
-        replace: false,
-        values: Object.values(this.segments).flat(Infinity),
-      })
-    );
-  }
-  reset(file) {
-    this.current = false;
-    delete this.segments[file];
-    if (!this.current) {
-      this.file.confirm();
-      this.current = true;
-    }
-    this.file.setContents(
-      JSON.stringify({
-        replace: false,
-        values: Object.values(this.segments).flat(Infinity),
-      })
-    );
-  }
+	constructor(path) {
+		this.segments = {}
+		this.file = new File()
+		this.file.setPath(path)
+		this.current = false
+	}
+	set(id, func) {
+		if (!this.current) {
+			this.file.confirm()
+			this.current = true
+		}
+		this.segments[id] = this.segments[id] || []
+		this.segments[id].push(func)
+		this.file.setContents(
+			JSON.stringify({
+				replace: false,
+				values: Object.values(this.segments).flat(Infinity),
+			})
+		)
+	}
+	reset(file) {
+		this.current = false
+		delete this.segments[file]
+		if (!this.current) {
+			this.file.confirm()
+			this.current = true
+		}
+		this.file.setContents(
+			JSON.stringify({
+				replace: false,
+				values: Object.values(this.segments).flat(Infinity),
+			})
+		)
+	}
 }
-const tickFile = new File();
+const tickFile = new File()
 tickFile.setPath(
-  path.resolve(
-    process.cwd(),
-    "./data/minecraft/functions/" + CONFIG.generatedDirectory + "/events/tick.mcfunction"
-  )
-);
-const tickFunction = new MultiFile(tickFile);
-const loadFile = new File();
+	path.resolve(process.cwd(), './data/minecraft/functions/' + CONFIG.generatedDirectory + '/events/tick.mcfunction')
+)
+const tickFunction = new MultiFile(tickFile)
+const loadFile = new File()
 loadFile.setPath(
-  path.resolve(
-    process.cwd(),
-    "./data/minecraft/functions/" + CONFIG.generatedDirectory + "/events/load.mcfunction"
-  )
-);
-const loadFunction = new MultiFile(loadFile);
+	path.resolve(process.cwd(), './data/minecraft/functions/' + CONFIG.generatedDirectory + '/events/load.mcfunction')
+)
+const loadFunction = new MultiFile(loadFile)
 
 class MCFunction extends File {
-  constructor(parent, top, intent) {
-    super();
-    this.parent = parent;
-    this.top = top || this;
-    this.functions = [];
-    this.namespace = "lang_error";
-    this._path = Math.random().toString(36).substr(2);
-    this.target = this;
-    this.intent = intent;
-  }
-  getHash() {
-    const c = crypto.createHash("md5").update(this.functions.join("\n"));
-    return c.digest("hex");
-  }
-  addCommand(command) {
-    this.functions.push(evaluate(command));
-  }
-  setPath(p) {
-    this._path = p;
-  }
-  getReference() {
-    return this.namespace + ":" + this._path;
-  }
-  getContents() {
-    return (
-      (CONFIG.header ? CONFIG.header + "\n\n" : "") +
-      this.functions
-        .map((command) =>
-          command
-            .replace(/\$block/g, this.namespace + ":" + this.getFunctionPath())
-            .replace(/\$top/g, this.top.getReference())
-            .replace(/\$parent/g, () => {
-              if (this.parent) {
-                return this.parent.getReference();
-              } else {
-                throw new CompilerError(
-                  "$parent used where there is no valid parent."
-                );
-              }
-            })
-        )
-        .join("\n")
-    );
-  }
-  getPath() {
-    return path.resolve(
-      process.cwd(),
-      "./data/",
-      this.namespace,
-      "./functions/",
-      this._path + ".mcfunction"
-    );
-  }
-  getFunctionPath() {
-    return this._path;
-  }
+	constructor(parent, top, intent) {
+		super()
+		this.parent = parent
+		this.top = top || this
+		this.functions = []
+		this.namespace = 'lang_error'
+		this._path = Math.random().toString(36).substr(2)
+		this.target = this
+		this.intent = intent
+	}
+	getHash() {
+		const c = crypto.createHash('md5').update(this.functions.join('\n'))
+		return c.digest('hex')
+	}
+	addCommand(command) {
+		this.functions.push(evaluate(command))
+	}
+	setPath(p) {
+		this._path = p
+	}
+	getReference() {
+		return this.namespace + ':' + this._path
+	}
+	getContents() {
+		return (
+			(CONFIG.header ? CONFIG.header + '\n\n' : '') +
+			this.functions
+				.map(command =>
+					command
+						.replace(/\$block/g, this.namespace + ':' + this.getFunctionPath())
+						.replace(/\$top/g, this.top.getReference())
+						.replace(/\$parent/g, () => {
+							if (this.parent) {
+								return this.parent.getReference()
+							} else {
+								throw new CompilerError('$parent used where there is no valid parent.')
+							}
+						})
+				)
+				.join('\n')
+		)
+	}
+	getPath() {
+		return path.resolve(process.cwd(), './data/', this.namespace, './functions/', this._path + '.mcfunction')
+	}
+	getFunctionPath() {
+		return this._path
+	}
 
-  confirm(file) {
-    if (!_fakefs.has(this._path)) {
-      _fakefs.add(this._path);
-      if (this.intent === "load") {
-        loadFunction.set(file, this.getReference());
-      } else if (this.intent === "tick") {
-        tickFunction.set(file, this.getReference());
-      }
-      super.confirm();
-    }
-  }
-  toString() {
-    return "function " + this.namespace + ":" + this.getFunctionPath();
-  }
+	confirm(file) {
+		if (!_fakefs.has(this._path)) {
+			_fakefs.add(this._path)
+			if (this.intent === 'load') {
+				loadFunction.set(file, this.getReference())
+			} else if (this.intent === 'tick') {
+				tickFunction.set(file, this.getReference())
+			}
+			super.confirm()
+		}
+	}
+	toString() {
+		return 'function ' + this.namespace + ':' + this.getFunctionPath()
+	}
 
-  static setEnv(_env) {
-    _fakefs = new Set();
-    env = _env;
-  }
+	static setEnv(_env) {
+		_fakefs = new Set()
+		env = _env
+	}
 }
 
 module.exports = {
-  MCFunction,
-  tickFunction,
-  loadFunction,
-  loadFile,
-  tickFile,
-  evaluate_str: evaluate,
-  MultiFileTag,
-};
+	MCFunction,
+	tickFunction,
+	loadFunction,
+	loadFile,
+	tickFile,
+	evaluate_str: evaluate,
+	MultiFileTag,
+}
--- transformlib.js
+++ transformlib.js
@@ -1,114 +1,113 @@
-const path = require("path");
-const SRC_DIR = path.resolve(process.cwd(), 'src');
-const LIB_JSON = path.resolve(process.cwd(), 'lib.json');
-const LIB_CONF = require(LIB_JSON);
-const UID = LIB_CONF.name + "-" + new Date().getTime().toString(36);
+const path = require('path')
+const SRC_DIR = path.resolve(process.cwd(), 'src')
+const LIB_JSON = path.resolve(process.cwd(), 'lib.json')
+const LIB_CONF = require(LIB_JSON)
+const UID = LIB_CONF.name + '-' + new Date().getTime().toString(36)
 
-module.exports = (context) => {
-    let files = {};
-    for (const source in context.vfs) {
-        const children = context.vfs[source];
-        for (const file in children) {
-            const content = children[file];
-            files[file.replace(/\\/g, "/")] = content;
-        }
-    }
-    const lib_name = LIB_CONF.name;
-    const json_files = Object.keys(files).filter((name) => name.endsWith(".json")).map(name => ({
-        name: lib_name + "/" + name,
-        content: files[name].replace(new RegExp("lib:", "g"), `lib:${lib_name}/`)
-    }));
-    const file_from_func = {};
-    let transforms = {};
-    let functions = [];
-    keys = Object.keys(files);
-    for (let func of keys) {
-        if (func.endsWith(".mcfunction")) {
-            const [, , , ...rest] = func.split("/");
-            const new_name = `data/lib/functions/${lib_name}/${rest.join("/")}`;
-            file_from_func[`lib:${lib_name}/${rest.join("/").replace(".mcfunction", "")}`] = new_name
-            files[new_name] = files[func];
-            delete files[func];
-            functions.push(`lib:${lib_name}/${rest.join("/").replace(".mcfunction", "")}`);
-        } else if (func.endsWith(".json")) {
-            files[lib_name + '/' + func] = files[func];
-            delete files[func];
-        }
-    }
-    const target = new RegExp("lib:", "g");
-    for (const name in files) {
-        files[name] = files[name].replace(target, `lib:${lib_name}/`);
-    }
-    const dependencies = {};
-    function getDeps(content) {
-        return functions.filter(
-            (func) => content.indexOf(func) != -1
-        );
-    }
-    const func_from_file = Object.fromEntries(Array.from(Object.entries(file_from_func)).map(([key, value]) => [value, key]))
-    for (let file in files) {
-        dependencies[file] = getDeps(files[file]);
-        if (dependencies[file].includes(func_from_file[file])) {
-            dependencies[file].splice(dependencies[file].indexOf(func_from_file[file]), 1);
-        }
-    }
-    function flatten(entries) {
-        const res = new Set();
-        const unseen = new Set(entries);
-        while (unseen.size > 0) {
-            const item = unseen.keys().next().value;
+module.exports = context => {
+	let files = {}
+	for (const source in context.vfs) {
+		const children = context.vfs[source]
+		for (const file in children) {
+			const content = children[file]
+			files[file.replace(/\\/g, '/')] = content
+		}
+	}
+	const lib_name = LIB_CONF.name
+	const json_files = Object.keys(files)
+		.filter(name => name.endsWith('.json'))
+		.map(name => ({
+			name: lib_name + '/' + name,
+			content: files[name].replace(new RegExp('lib:', 'g'), `lib:${lib_name}/`),
+		}))
+	const file_from_func = {}
+	let transforms = {}
+	let functions = []
+	keys = Object.keys(files)
+	for (let func of keys) {
+		if (func.endsWith('.mcfunction')) {
+			const [, , , ...rest] = func.split('/')
+			const new_name = `data/lib/functions/${lib_name}/${rest.join('/')}`
+			file_from_func[`lib:${lib_name}/${rest.join('/').replace('.mcfunction', '')}`] = new_name
+			files[new_name] = files[func]
+			delete files[func]
+			functions.push(`lib:${lib_name}/${rest.join('/').replace('.mcfunction', '')}`)
+		} else if (func.endsWith('.json')) {
+			files[lib_name + '/' + func] = files[func]
+			delete files[func]
+		}
+	}
+	const target = new RegExp('lib:', 'g')
+	for (const name in files) {
+		files[name] = files[name].replace(target, `lib:${lib_name}/`)
+	}
+	const dependencies = {}
+	function getDeps(content) {
+		return functions.filter(func => content.indexOf(func) != -1)
+	}
+	const func_from_file = Object.fromEntries(
+		Array.from(Object.entries(file_from_func)).map(([key, value]) => [value, key])
+	)
+	for (let file in files) {
+		dependencies[file] = getDeps(files[file])
+		if (dependencies[file].includes(func_from_file[file])) {
+			dependencies[file].splice(dependencies[file].indexOf(func_from_file[file]), 1)
+		}
+	}
+	function flatten(entries) {
+		const res = new Set()
+		const unseen = new Set(entries)
+		while (unseen.size > 0) {
+			const item = unseen.keys().next().value
 
-            if (!res.has(item)) {
-                const children = dependencies[
-                    file_from_func[item]
-                ]
-                for (let i = 0; i < children.length; i++) {
-                    if (!res.has(file_from_func[children[i]])) {
-                        unseen.add(children[i]);
-                    }
-                }
-            }
-            unseen.delete(item);
-            res.add(item);
-        }
-        return Array.from(res.keys());
-    }
-    for (let i = 0; i < json_files.length; i++) {
-        json_files[i].dependencies = flatten(getDeps(json_files[i].content)).map(_ => file_from_func[_]);
-    }
-    if (context.ext === ".mcm") {
-        const entry = path.join('__MACRO_METADATA__', path.resolve(SRC_DIR, context.file)).replace(/\\/g, "/");
-        const data = JSON.parse(files[entry]);
-        const output = {};
-        const load_json = Object.keys(files).find(file => file.endsWith("load.json"));
-        const tick_json = Object.keys(files).find(file => file.endsWith("tick.json"));
-        for (const name in data) {
-            output[name] = {
-                tokens: data[name].map((item) => {
-                    item.line = "lib/" + LIB_CONF.name;
-                    item.file = "lib/" + LIB_CONF.name;
-                    return item;
-                }),
-                dependencies: [
-                    ...flatten(getDeps(data[name].map(({ token }) => token).join("\n"))).map(_ => file_from_func[_]),
-                    load_json,
-                    tick_json
-                ].filter(Boolean)
-            }
-        }
-        return { macros: output, json: json_files };
-    } else {
-        const current = context.vfs[path.join("src", context.file)];
-        const output = {};
-        for (const func in files) {
-            if (func.endsWith(".mcfunction")) {
-                output[func] = {
-                    content: files[func],
-                    dependencies: flatten(getDeps(files[func])).map(_ => file_from_func[_])
-                }
-            }
-        }
-        return { functions: output, json: json_files };
-    }
-
+			if (!res.has(item)) {
+				const children = dependencies[file_from_func[item]]
+				for (let i = 0; i < children.length; i++) {
+					if (!res.has(file_from_func[children[i]])) {
+						unseen.add(children[i])
+					}
+				}
+			}
+			unseen.delete(item)
+			res.add(item)
+		}
+		return Array.from(res.keys())
+	}
+	for (let i = 0; i < json_files.length; i++) {
+		json_files[i].dependencies = flatten(getDeps(json_files[i].content)).map(_ => file_from_func[_])
+	}
+	if (context.ext === '.mcm') {
+		const entry = path.join('__MACRO_METADATA__', path.resolve(SRC_DIR, context.file)).replace(/\\/g, '/')
+		const data = JSON.parse(files[entry])
+		const output = {}
+		const load_json = Object.keys(files).find(file => file.endsWith('load.json'))
+		const tick_json = Object.keys(files).find(file => file.endsWith('tick.json'))
+		for (const name in data) {
+			output[name] = {
+				tokens: data[name].map(item => {
+					item.line = 'lib/' + LIB_CONF.name
+					item.file = 'lib/' + LIB_CONF.name
+					return item
+				}),
+				dependencies: [
+					...flatten(getDeps(data[name].map(({ token }) => token).join('\n'))).map(_ => file_from_func[_]),
+					load_json,
+					tick_json,
+				].filter(Boolean),
+			}
+		}
+		return { macros: output, json: json_files }
+	} else {
+		const current = context.vfs[path.join('src', context.file)]
+		const output = {}
+		for (const func in files) {
+			if (func.endsWith('.mcfunction')) {
+				output[func] = {
+					content: files[func],
+					dependencies: flatten(getDeps(files[func])).map(_ => file_from_func[_]),
+				}
+			}
+		}
+		return { functions: output, json: json_files }
+	}
 }
